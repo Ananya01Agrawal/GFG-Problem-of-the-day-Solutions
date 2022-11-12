e83dfb5312433ef5622c12c262de0996ce3d734a
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2022-11-12 07:36:25 +0530

    Create Print Matrix in snake Pattern.java

---
+++ matrix/Print Matrix in snake Pattern.java
@@ -0,0 +1,26 @@
+class Solution
+{
+    //Function to return list of integers visited in snake pattern in matrix.
+    static ArrayList<Integer> snakePattern(int matrix[][])
+    {
+        // code here 
+        int left=0;
+        int right=matrix.length;
+        ArrayList<Integer>list=new ArrayList<>();
+        for(int i=0;i<matrix.length;i++){
+            if(i%2==0){
+                for(int j=0;j<right;j++){
+                    list.add(matrix[i][j]);
+                    
+                }
+            }
+            else{
+                for(int j=right-1;j>=left;j--){
+                    list.add(matrix[i][j]);
+                }
+            }
+        }
+        return list;
+    }
+    
+}
