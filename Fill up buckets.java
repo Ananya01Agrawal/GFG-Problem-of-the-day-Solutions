095881d04dadf4de586642965b99c47c52fe5a3e
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2022-11-13 10:26:38 +0530

    Create Fill up buckets.java

---
+++ Fill up buckets.java
@@ -0,0 +1,26 @@
+class Solution{
+    public int totalWays(int n, int[] capacity) {
+        // code here
+        Arrays.sort(capacity);
+
+        long ans = 1;
+
+        long mod = 1000000007;
+
+        int fill = 0;
+
+        for(int i = 0; i < n; i++){
+
+            ans = ans * (capacity[i] - fill);
+
+            ans = ans%mod;
+
+            fill++;
+
+        }
+
+        return (int)ans;
+
+    
+    }
+}
