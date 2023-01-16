9c19ac1558b4c1f26a5713a2472a68297d0468c9
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2023-01-16 21:15:57 +0530

    Create Next Greater Element.java

---
+++ Next Greater Element.java
@@ -0,0 +1,27 @@
+class Solution{
+    public static long[] nextLargerElement(long[] arr, int n) { 
+        // Your code here
+        long a[]=new long[n];
+
+        Stack<Long>s=new Stack<>();
+
+        for(int i=n-1;i>=0;i--)
+
+        {
+
+             while(!s.isEmpty() && s.peek()<arr[i])s.pop();
+            if(s.isEmpty())
+
+            a[i]=-1;
+
+            else 
+
+            a[i]=s.peek(); 
+
+            s.push(arr[i]);
+
+  }
+
+   return a;
+    } 
+}
