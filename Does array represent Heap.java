6929960fe230b7ead9bdea448af93b4303f1d300
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2024-02-17 08:08:16 +0530

    Create Does array represent Heap.java

---
+++ Does array represent Heap.java
@@ -0,0 +1,15 @@
+class Solution {
+    
+    public boolean countSub(long arr[], long n)
+    {
+        // Your code goes here
+        for(int i=0;i<(int)n;i++){
+            if(i*2+1 <(int)n)
+                if(arr[i]<arr[i*2+1]) return false;
+            
+            if(i*2+2 <(int)n)
+                if(arr[i]<arr[i*2+2]) return false;
+        }
+        return true;
+    }
+}
